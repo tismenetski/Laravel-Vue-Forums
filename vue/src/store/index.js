be594e9d434d30c9ae7b69cb7336ffd0e7c251cb
tismenetski <tismenetski@gmail.com>
2022-02-05 12:51:03 +0200

    Frontend : store actions

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,5 +1,5 @@
 import {createStore} from "vuex";
-
+import axiosClient from "../axios";
 const store = createStore(
     {
         state : {
@@ -9,12 +9,33 @@ const store = createStore(
                     age: '35',
                     imageUrl : 'https://media.npr.org/assets/img/2014/08/07/monkey-selfie_custom-7117031c832fc3607ee5b26b9d5b03d10a1deaca.jpg'
                 },
-                token : null
+                token : sessionStorage.getItem('TOKEN')
             }
         },
         getters : {},
-        actions : {},
-        mutations : {},
+        actions : {
+            register({commit}, user) {
+                return axiosClient.post('/register', user)
+                    .then(({data}) => {
+                        commit('setUser', data);
+                        return data;
+                    })
+            },
+            login({commit}, user) {
+                return axiosClient.post('/login', user)
+                    .then(({data}) => {
+                        commit('setUser', data);
+                        return data;
+                    })
+            }
+        },
+        mutations : {
+            setUser : (state,userData) => {
+                state.user.token = userData.token;
+                state.user.data = userData.user;
+                sessionStorage.setItem('TOKEN' , userData.token);
+            },
+        },
         modules : {}
     }
 )
