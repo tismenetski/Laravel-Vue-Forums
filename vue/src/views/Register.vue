489494605a0270b4d31d75c852f87c3615f60820
tismenetski <tismenetski@gmail.com>
2022-02-05 12:49:58 +0200

    Frontend : Auth components

---
+++ vue/src/views/Register.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div class="max-w-md w-full space-y-8">
+            <div>
+                <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
+                <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Register New Account</h2>
+                <p class="mt-2 text-center text-sm text-gray-600">
+                    Or
+                    {{ ' ' }}
+                    <router-link :to="{name : 'login'}" class="font-medium text-indigo-600 hover:text-indigo-500"> Login </router-link>
+                </p>
+            </div>
+            <form class="mt-8 space-y-6" @submit="register">
+                <input type="hidden" name="remember" value="true" />
+                <div class="rounded-md shadow-sm -space-y-px">
+                    <div>
+                        <label for="name" class="sr-only">Name</label>
+                        <input v-model="user.name" id="name" name="name" type="text" autocomplete="name" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Your Name" />
+                    </div>
+                    <div>
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input v-model="user.email" id="email-address" name="email" type="email" autocomplete="email" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
+                    </div>
+                    <div>
+                        <label for="password" class="sr-only">Password</label>
+                        <input v-model="user.password" id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
+                    </div>
+                    <div>
+                        <label for="password_confirmation" class="sr-only">Confirm Password</label>
+                        <input v-model="user.password_confirmation" id="password_confirmation" name="password_confirmation" type="password" autocomplete="password_confirmation" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Re-Enter Password" />
+                    </div>
+                </div>
+
+                <div class="flex items-center justify-between">
+                    <div class="text-sm">
+                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </a>
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+              <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+            </span>
+                        Register
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { LockClosedIcon } from '@heroicons/vue/solid'
+import {ref} from "vue";
+import {useStore} from "vuex";
+import {useRouter} from "vue-router";
+
+const store = useStore();
+const router = useRouter();
+
+const user  = {
+    name : '',
+    email : '',
+    password : '',
+    password_confirmation : ''
+};
+
+const errors = ref({});
+
+
+function register(ev) {
+    ev.preventDefault();
+    store.dispatch('register', user).then(() => {
+            router.push('/');
+    }).catch((err) => {
+        throw err;
+    })
+
+
+}
+
+
+
+
+</script>
