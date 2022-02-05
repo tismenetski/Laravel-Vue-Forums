189e126c50f655c09c4c611c53e1bf9bf7c012a4
tismenetski <tismenetski@gmail.com>
2022-02-05 12:50:13 +0200

    Frontend : Routes

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,6 +2,8 @@ import {createRouter,createWebHistory} from "vue-router";
 
 
 import Dashboard from "../views/Dashboard.vue";
+import Register from "../views/Register.vue";
+import Login from "../views/Login.vue";
 
 
 const routes = [
@@ -9,6 +11,16 @@ const routes = [
         path : '/',
         name  : 'dashboard',
         component : Dashboard
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: Register
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login
     }
 ]
 
