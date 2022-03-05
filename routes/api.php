e2cc6b9ed96f751dea4c0a9817c065ca2f2f156a
tismenetski <tismenetski@gmail.com>
2022-03-05 12:42:53 +0200

    rearrenged the api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ReplyController;
 use App\Http\Controllers\TopicController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,38 +20,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->group(function (){
+Route::post('/register' , [AuthController::class,'register']);
+Route::post('/login' , [AuthController::class,'login']);
 
+Route::middleware('auth:sanctum')->group(function (){
     Route::get('/user', function (Request $request){
         return $request->user();
     });
-
-
     Route::post('/logout', [AuthController::class,'logout']);
-
 });
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//
-//
-//});
+Route::controller(CategoryController::class)->prefix('categories')->group(function (){
+    Route::get('/', [CategoryController::class,'index']);
+    Route::get('/{id}' , [CategoryController::class,'show']);
+});
 
+Route::controller(TopicController::class)->prefix('topics')->group(function (){
+    Route::get('/{category_id}', [TopicController::class,'index']);
+    Route::post('/' , [TopicController::class, 'create']);
+    Route::delete('/topic/{id}', [TopicController::class,'destroy']);
+});
 
-Route::get('/categories', [CategoryController::class,'index']);
-Route::get('/categories/{id}' , [CategoryController::class,'show']);
+Route::controller(PostController::class)->prefix('posts')->group(function (){
+    Route::get('/{topic_id}' , [PostController::class, 'index']);
+    Route::middleware('auth:sanctum')->post('/' , [PostController::class, 'create']);
+    Route::get('/post/{id}', [PostController::class, 'show']);
+    Route::middleware('auth:sanctum')->put('/post/{id}' , [PostController::class, 'update']);
+    Route::middleware('auth:sanctum')->delete('/post/{id}', [PostController::class,'destroy']);
+});
 
-Route::get('/topics/{category_id}', [TopicController::class,'index']);
-Route::post('/topics' , [TopicController::class, 'create']);
-Route::delete('/topics/topic/{id}', [TopicController::class,'destroy']);
+Route::controller(CommentController::class)->prefix('comments')->group(function (){
+    Route::middleware('auth:sanctum')->post('/', [CommentController::class,'create']);
+});
 
-Route::get('/posts/{topic_id}' , [PostController::class, 'index']);
-Route::middleware('auth:sanctum')->post('/posts' , [PostController::class, 'create']);
-Route::get('posts/post/{id}', [PostController::class, 'show']);
-Route::middleware('auth:sanctum')->put('posts/post/{id}' , [PostController::class, 'update']);
-Route::middleware('auth:sanctum')->delete('posts/post/{id}', [PostController::class,'destroy']);
-
-
-
-Route::post('/register' , [AuthController::class,'register']);
-Route::post('/login' , [AuthController::class,'login']);
+Route::controller(ReplyController::class)->prefix('replies')->group(function (){
+    Route::middleware('auth:sanctum')->post('/', [ReplyController::class,'create']);
+    Route::middleware('auth:sanctum')->delete('/reply/{id}', [ReplyController::class,'destroy']);
+    Route::middleware('auth:sanctum')->put('/reply/{id}', [ReplyController::class,'update']);
+});
