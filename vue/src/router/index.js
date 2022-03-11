49470774a71266ae13ab8b46c8511cd33871f48f
tismenetski <tismenetski@gmail.com>
2022-03-11 14:54:17 +0200

    Routes

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,6 +4,9 @@ import {createRouter,createWebHistory} from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Register from "../views/Register.vue";
 import Login from "../views/Login.vue";
+import Forums from '../views/Forums.vue';
+import Post from '../views/Post.vue';
+import Topic from "../views/Topic.vue";
 
 
 const routes = [
@@ -21,7 +24,24 @@ const routes = [
         path: '/login',
         name: 'login',
         component: Login
-    }
+    },
+    {
+        path: '/forums',
+        name: 'forums',
+        component: Forums
+    },
+    {
+        path: '/topic/:id',
+        name: 'topic',
+        component: Topic
+    },
+    {
+        path: '/post/:id',
+        name: 'post',
+        component: Post
+    },
+
+
 ]
 
 
--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -10,6 +10,13 @@ const store = createStore(
                     imageUrl : 'https://media.npr.org/assets/img/2014/08/07/monkey-selfie_custom-7117031c832fc3607ee5b26b9d5b03d10a1deaca.jpg'
                 },
                 token : sessionStorage.getItem('TOKEN')
+            },
+            posts : {
+                topPosts : []
+            },
+            topics :{
+                topics : [],
+                topic : null
             }
         },
         getters : {},
@@ -27,6 +34,27 @@ const store = createStore(
                         commit('setUser', data);
                         return data;
                     })
+            },
+            topPosts({commit}) {
+                return axiosClient.get('/posts/top/top_posts')
+                    .then(({data}) => {
+                        commit('setTopPosts', data);
+                        return data;
+                    })
+            },
+            topics({commit}) {
+                return axiosClient.get('/topics/topic/all')
+                    .then(({data})=> {
+                        commit('setTopics', data);
+                        return data;
+                    })
+            },
+            getTopic({commit}, id) {
+                return axiosClient.get(`/topics/topic/${id}`)
+                    .then(({data}) => {
+                        commit('setTopic', data);
+                        return data;
+                    })
             }
         },
         mutations : {
@@ -35,6 +63,15 @@ const store = createStore(
                 state.user.data = userData.user;
                 sessionStorage.setItem('TOKEN' , userData.token);
             },
+            setTopPosts(state,postsData){
+                state.posts.topPosts = postsData;
+            },
+            setTopics(state,topicData) {
+                state.topics.topics = topicData;
+            },
+            setTopic(state,topicData) {
+                state.topics.topic = topicData;
+            }
         },
         modules : {}
     }
