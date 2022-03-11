9f7932275fafce6393ac9274e920c1006983a8ac
tismenetski <tismenetski@gmail.com>
2022-03-11 14:52:21 +0200

    Added font-awesome

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,7 +3,18 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import './index.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {fas, faThumbTack} from '@fortawesome/free-solid-svg-icons'
+import {faComment} from "@fortawesome/free-solid-svg-icons";
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(faThumbTack)
+library.add(faComment)
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas, far, fab)
 createApp(App)
     .use(store)
     .use(router)
+    .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
