75859edf19f73b5af0558cee33736e77fde7f22d
tismenetski <tismenetski@gmail.com>
2022-04-01 09:30:47 +0300

    New routes for votes

--- routes/api.php
+++ routes/api.php
@@ -51,14 +51,17 @@ Route::controller(PostController::class)->prefix('posts')->group(function (){
     Route::middleware('auth:sanctum')->delete('/post/{id}', [PostController::class,'destroy']);
     Route::middleware('auth:sanctum')->get('/user/user_posts',[PostController::class,'user_posts']);
     Route::get('/top/top_posts' , [PostController::class,'top_posts']);
+    Route::middleware('auth:sanctum')->post('/vote' , [PostController::class,'vote']);
 });
 
 Route::controller(CommentController::class)->prefix('comments')->group(function (){
     Route::middleware('auth:sanctum')->post('/', [CommentController::class,'create']);
+    Route::middleware('auth:sanctum')->post('/vote', [CommentController::class,'vote']);
 });
 
 Route::controller(ReplyController::class)->prefix('replies')->group(function (){
     Route::middleware('auth:sanctum')->post('/', [ReplyController::class,'create']);
     Route::middleware('auth:sanctum')->delete('/reply/{id}', [ReplyController::class,'destroy']);
     Route::middleware('auth:sanctum')->put('/reply/{id}', [ReplyController::class,'update']);
+    Route::middleware('auth:sanctum')->post('/vote', [ReplyController::class,'vote']);
 });
